1415d22f792b86538255fd4564b8d6f6f4a17fde
Gerardo Pérez <gerard2perez@outlook.com>
2015-01-02 22:27:37 -0600

    REST response fix

--- public/js/app/models.js
+++ public/js/app/models.js
@@ -1,5 +1,8 @@
 EMPapp.Employee = DS.Model.extend({
     Name: DS.attr('string'),
 	LastName: DS.attr('string'),
-	email:DS.attr('string')
+	email:DS.attr('string'),
+	FullName:function(){
+		return ( ( this.get('Name') || '') + ' '+(this.get('LastName') || '') ).trim();
+	}.property ('Name', 'LastName')
 });
--- routes/employee.js
+++ routes/employee.js
@@ -5,7 +5,7 @@ var db = require('../models');
 router.get('/', function(req, res) {
 	
 	db.employee.find(function(err,data,count){
-		res.send(JSON.stringify({ employees:data }));
+		res.send(JSON.stringify({ employees:data }).replace(/_id/g,"id"));
 	});
 });
 
@@ -14,7 +14,7 @@ router.post('/',function(req,res){
 	req.body.employee.updateddAt = new Date();
 	console.log(req.body.employee);
 	new db.employee(req.body.employee).save(function(err,data,count){
-		res.send(JSON.stringify(data));
+		res.send(JSON.stringify({employee:data}).replace(/_id/g,"id"));
 	});
 });
 
@@ -25,7 +25,7 @@ router.put('/:id',function(req,res){
 		record.email = req.body.employee.email;
 		record.updateddAt = new Date();
 		record.save(function(err,data,count){
-			res.send(JSON.stringify(data));
+			res.send(JSON.stringify({employee:data}).replace(/_id/g,"id"));
 		});
 	});
 	
@@ -34,7 +34,7 @@ router.put('/:id',function(req,res){
 router.delete('/:id',function(req,res){
 	db.employee.findById(req.params.id,function(err,record){
 		record.remove();
-		res.send(JSON.stringify(record));
+		res.send(JSON.stringify({employee:record}).replace(/_id/g,"id"));
 	});
 });
 
